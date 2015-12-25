171dba0e143b507b6e4597d4b55a983325cb44e7
tifred <tifred@lmi.net>
2015-12-25 10:44:17 -0800

    added second column of text and all the text itself

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,9 @@ var initialSeries = [
     seriesName: "skate",
     mainImgIdx: 1,
     artImgIdx: 2,
-    artTitle: "SKATING",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artTitle: "LIFE SUCKS",
+    artText: '<p><strong>I HAPPENED UPON THIS SKATEPARK </strong>when it was being built on a long abandoned lot in West Oakland, right next to a freeway and the warehouse where I was photographing Bob Rosenberg (see my West Oakland gallery). There turned out to be no permissions, no permits,&nbsp; and no one getting paid. It was part of a DIY,&nbsp; Build-a-Skatepark movement that started in Portland Oregon in l990, with the cost of materials donated by skateboard magazines and sponsors. The idea is for skaters and their supporters in the construction business to build a park fast (this one in just under 5 months), open it up for public use (put a hole in the chain-link fence the city puts up) and hope the city doesn\'t tear it down.</p><p>I loved watching the skaters, of course, but ended up being more interested in a culture I hadn\'t really seen as a girl and an only child. There isn\'t really a word for it other than "testosterone," or "boys will be boys." In the skating world it\'s mostly alienated urban kids whose mottos are "freedom" and "life sucks." They want to fall hard, feel free, skate well and get high. I found a brotherhood of kids feeling strong and brave and happy together,&nbsp; if only for a time.</p>',
+    artTextTwo: '',
     images: [
       {
         img: "skate_0",
@@ -30,7 +31,8 @@ var initialSeries = [
     mainImgIdx: 0,
     artImgIdx: 1,
     artTitle: "WEST OAKLAND",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>I STARTED PHOTOGRAPHING WEST OAKLAND </strong>on its outer edges because I had no idea what was there and I love to explore. I had spent a lot of time in third world countries but not in the poor and forgotten areas of my own home town. The first thing I found near the train tracks and freeways were the homeless, the addicts, and the recycling underworld — people living in“deep poverty," which is officially defined as less than $8.36 a day.&nbsp; Heading just a few blocks inland I found West Oakland\'s old black community — the men and women whose families had fled rural Louisiana and Texas during WWII to work in the booming railroads and shipyards. These folk were easy to find because many of the family homes and apartments had become so crowded daily life spilled into the streets.&nbsp;</p><p>Gentrification is not new, but it\'s now on hyper-speed in West Oakland due to white and Asian workers in San Francisco flooding in. They themselves are usually refugees — from a city whose "affordable housing" goes to families making up to $150,000 a year.&nbsp; Large condo developments are going up in West Oakland, faux Victorian houses are being built, and a dilapidated Victorian fixer-upper can go for a half a million dollars. Many of the old manufacturing plantsare being turned into beautiful but pricey lofts. The mostly young, white, artists and BART commuters moving in say they love the diversity,&nbsp; the good weather and the fun, urban hipster vibe. But where is a grocery store? And why is trash dumped everywhere?&nbsp; And what can a liberal white person do about all the crime and black-on-black homicides?</p><p><strong>A BRIEF HISTORY</strong>: At the turn of the century West Oakland was blessed. It was the terminal of the transcontinental railroad and where the ferry to San Francisco took off. It\'s grand old Victorians were built in the late 1800s by European immigrants, mostly Italians and Portuguese.&nbsp; After the l906 earthquake in San Francisco a lot of smaller Victorians were built in West Oakland as well as parks and schools. In the l930s the neighborhood went through its first change when most of it became zoned for manufacturing.&nbsp; During WWII West Oakland changed again as African-Americans flooded in. Old-timers say the best time in West Oakland was in the l940s and \'50s when it was a crowded, bustling, majority African American boom town. There was racism and red-lining and neglect by politicians, but unions were strong and the money was good and the churches and schools and nightclubs flourished.&nbsp; Women worked, often in white people\'s homes, but they had their own local clubs and associations.</p>',
+    artTextTwo: '<p>&nbsp;The blues came to West Oakland via the Pullman porters who traveled to Chicago and New York, and restaurants, bars and nightclubs followed. Clubs like Slim Jenkins hosted big names — Aretha Franklin, Ray Charles, Ike and Tina Turner — and the California Hotel became a gathering spot for the black musicians and performers who weren\'t welcome in San Francisco.</p><p>The neighborhood started to decline in the l950\'s when a freeway cut through it and manufacturing jobs dried up. Urban renewal projects and a regional post office destroyed hundreds of the old Victorian homes. In 1966 The Black Panther Party was born in West Oakland with it\'s message of self-defense against police brutality. The Panthers evolved to sponsor local, self-help programs, and for a time had offices in cities all over the US. Then political in-fighting weakened the movement plus harassment and incarceration by the FBI. The end of the Panthers came when Huey Newton was shot by his drug dealer and Eldrige Cleaver went off the rails and supported Ronald Regan. The blues clubs lasted until the l970\'s when noise from the new BART train wiped them out. In the late l980s and l990s,&nbsp; a crack epidemic bottomed out the community and the neighborhood hit rock-bottom. Today drug-use is down and schools have improved but there still is a school-to-prison pipeline. There is also a lot of friendliness and good will that remains in the neighborhood, and on a summer day parts of West Oakland feel like a small town in the rural South.</p>',
     images: [
       {
         img: "westoak_0",
@@ -54,7 +56,8 @@ var initialSeries = [
     mainImgIdx: 2,
     artImgIdx: 1,
     artTitle: "AMONG THE HOMELESS",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>I\'VE SEEN HOMELESS PEOPLE</strong>, of course, but until I approached them with a camera I had never studied their faces; never heard their stories; never understood that no one now, not even mothers, has a safety net. Today a mother of a single child gets less than $400 a month and that for only <em>four years during her entire lifetime!</em></p><p>Some people in this gallery are homeless by choice – the kids, for instance, who ride the rails. They drink a lot and live in packs but when it stops being fun they go home. Others are homeless not by choice but because they can\'t hold a job due to mental illness or addiction. And even if they qualify for the highest amount of disability welfare, SSI, (around $889-a-month), it\'s not enough to pay for rent in the Bay Area, no less clothing, utilities and transportation. Mothers, I found out, have even less support. After the welfare-to-work program passed in l996 a mother of four will get less than $900 a month for a maximum of four years <em>during her entire lifetime!&nbsp;</em> Felons coming out of jail or prison have a particularly hard time as they are banned from staying with anyone, even familly or friends,&nbsp; who live in most kinds of low-income housing. For some felons the risk of going back to prison makes more sense than living on handouts.</p><p>Still, most homeless people aren\'t criminals: they are too sorry or too old for that. They don\'t victimize; they are victims. And when you know their childhood stories and medical histories it\'s pretty clear they never had a chance.</p>',
+    artTextTwo: '<em>I never photograph people without asking for permission and if they\'re homeless I may give them a few bucks. My attitude towards giving money to homeless people is unambiguous: I do it because I know it will help them get through their day.&nbsp;&nbsp;</em>',
     images: [
       {
         img: "homeless_0",
@@ -78,7 +81,8 @@ var initialSeries = [
     mainImgIdx: 0,
     artImgIdx: 1,
     artTitle: "RUST AMONG US",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>I\'VE ALWAYS BEEN</strong><strong> ATTRACTED </strong>to the pipes and wheels of old machines, so of course I discovered rust. Iron changes shape as it comes into contact with oxygen; paint peels and bubbles and comes off.&nbsp; With time, all of it looks great.</p><p>I\'m not sure why rust seems to go with every other color, but it does — even orange. And people seem to understand that machines will decay looking good if they paint them with the brightest of primary colors.&nbsp;<br><br>&nbsp;</p>',
+    artTextTwo: '',
     images: [
       {
         img: "rust_0",
@@ -102,7 +106,8 @@ var initialSeries = [
     mainImgIdx: 1,
     artImgIdx: 2,
     artTitle: "ON THE ROAD",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>T</strong><strong>HESE ARE SOME</strong> <strong>PHOTOS</strong> from my local wanderings and desert road trips through Utah, Nevada and California. I love that my camera gives me an excuse to go exploring in this wonderful part of the world.&nbsp; I ended up talking to a lot of guys like this one -- friendly people with time on their hands who hold forth in local bars.&nbsp; To the right of him, on the bar, is my road map and car keys. A great way to meet locals is to drive down back roads and not know where you end up.&nbsp;</p>',
+    artTextTwo: '',
     images: [
       {
         img: "road_0",
@@ -126,7 +131,8 @@ var initialSeries = [
     mainImgIdx: 0,
     artImgIdx: 1,
     artTitle: "AT THE CHURCH",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>THESE ARE THE FACES FROM THREE "OLD SCHOOL"” </strong>churches in West Oakland — snapshots taken on Easter Sunday, 2015. They are the faces of the black community\'s "solid citizens." The men in these photos have probably always worked; the women have either worked or been active in church and volunteer organizations. Most are well educated. Their roots go back to Texas or Louisiana, the southern end of the railroad line that took their families to California during WWII in search of jobs. In the l940s, in Jim Crow south, it was almost impossible for African-Americans to succeed. Segregation wasn\'t just about not being able to vote or having separate schools. It was about having to step off the sidewalk to let a white person pass. None of these solid citizens say racism is over because too many black people are in desperate straits.</p>',
+    artTextTwo: '',
     images: [
       {
         img: "easter_0",
@@ -150,7 +156,8 @@ var initialSeries = [
     mainImgIdx: 1,
     artImgIdx: 0,
     artTitle: "IN KANO",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>KANO IS CLOSE TO MY HEART</strong>. It\'s crowded, hot, dusty and ugly, but I spent four happy months there during three visits in the l990s, researching a book about marriage. I loved the people and their willingness to laugh and enjoy good conversation and I was in awe of their effortless social graces. When I couldn\'t finish my book (see About Me) I went back to Kano in 2010,&nbsp; this time with a camera looking for visual narratives. Kano was still fairly safe and I could walk around freely with my camera but people\'s attitude towards me had changed.&nbsp; America had just helped the French bomb Libya, a Muslim country whose leader favored black Africans over Arabs. The US press thought it was a good idea and I didn\'t know any better. Also, most people suspected that I was, like most journalists, there to get stories only about disease, corruption and poverty.</p><p>An Islamic fundamentalist terrorist group had just formed — Boko Haram — but no one wanted to talk about it. I couldn\'t figure out why until I realized even my educated Nigerian friend thought the CIA was behind it: we wanted to destabilize Nigeria and take its oil.<br><br>I hired two guides part-time since there were no rental cars and driving had become seriously chaotic. One was the editor of a weekly English language newspaper and the “head” of a private secondary school run by his wife. (This will give you some idea of the state of the economy: He needed a third job.) The other was a guy who carved out a living working for the academics who come to Kano, usually for months at a time. Both men were smart and likable but only one had a car. This meant I spent time on the back of his motorcycle, stalled in traffic, in a clearly overpopulated city of maybe six million people, breathing in a lifetime of exhaust.</p><p>As for the conservative Islam practiced by the Hausa people — I was impressed by it in the l990s. All religions encourage people to be good and do good, and Islam had managed to keep Northern Nigeria a lot safer than the South. Traditional rulers and Islamic teachers solved a lot of conflicts before they became violent or ended up in corrupt, western-style, courts. The attitude towards Christians did worry me, as did prejudice against other tribes.</p>',
+    artTextTwo: '<p>The question of women\'s rights was more complicated. Hausa society was, and still is, very patriarchal and hierarchical: sons and daughters must obey their elders and women must obey their husbands. If a husband divorces his wife (which happens frequently) she will get support from her family but she must obey her fathers or elder brothers. Many women in northern Nigeria still live in "traditional households" where they must get permission from their husbands before leaving the house. But there is one good sign for women\'s rights and that is education. Because Nigerian Muslims believe that Islam encourages education, just about every boy and girl in Kano, and even in the rural villages, wants to go to school. Girls are flooding into the universities as well, and in the high school my guide and his wife run, there are more girls than boys. Whether the educational system can handle the influx is another question. For whatever reason, the quality of education in Kano\'s Bayero University has declined. It could be that more students are coming from traditional and rural schools where the main curriculum is memorizing the Koran. And since elders are the ultimate authority on everything in Kano, students often don\'t know how to ask questions and challenge answers.</p>',
     images: [
       {
         img: "kano_0",
@@ -174,7 +181,8 @@ var initialSeries = [
     mainImgIdx: 2,
     artImgIdx: 1,
     artTitle: "AT THE GRAIN MARKET",
-    artText: "<p>Text</p><p>TEXT</p>",
+    artText: '<p><strong>I WENT TO NIGERIA IN 2010 </strong>looking for something good in Africa that was not set up by a foreign NGO. What I found was a huge grain market just outside Kano in Muslim Northern Nigeria. I was there at the end of the four-month rainy season when enormous trucks were coming in from as far away as Mali and Ghana and leaving with sacks of sorghum, peanuts, millet and maize. The Hausa people who dominate the area are known to be good farmers and traders and as far as I could see the market was well-run. It had a chairman and elected officers and five separate market associations. The workers had a union and were happy to have a job even it it paid only a few dollars a day. As for corruption, it was surely there, but of course I couldn\'t see it.&nbsp;</p><p>Because cassava and maize were milled in the market I photographed the white flour dust as it settled on black faces—the reverse of those iconic American images of black coal dust on white faces. Breathing in flour isn\'t as bad as breathing in coal, but when I got home and showed some of these photos to friends they asked a lot of questions about what would happen to their lungs.&nbsp;</p><p>I have no photos of the market at night because various association heads shooed me away. I was told theft wasn\'t a big problem and prostitutes had been kicked out of the trucking area when Shariah came to Kano in 2000, so maybe the problem was that I was a woman in a patriarchal society and my place, especially at night, was with my family in my home.&nbsp;</p>',
+    artTextTwo: '',
     images: [
       {
         img: "grainmkt_0",
@@ -207,6 +215,7 @@ var Series = function(data) {
   this.artAltText = data.images[data.artImgIdx].altText;
   this.artTitle = data.artTitle;
   this.artText = data.artText;
+  this.artTextTwo = data.artTextTwo;
 
   this.mainImgPath = "images/" + this.seriesName + "/" + this.mainImg;
   this.mainSrc = this.mainImgPath + "-640.jpg";
@@ -262,6 +271,7 @@ var ViewModel = function() {
   this.galleryIsVisible = ko.observable(false);
   this.artTitle = ko.observable("");
   this.artText = ko.observable("");
+  this.artTextTwo = ko.observable("");
   this.artSrc = ko.observable("");
   this.artSrcset = ko.observable("");
   this.artSizes = ko.observable("");
@@ -293,6 +303,7 @@ var ViewModel = function() {
 
     self.artTitle(series.artTitle);
     self.artText(series.artText);
+    self.artTextTwo(series.artTextTwo);
     self.artSrc(series.artSrc);
     self.artSrcset(series.artSrcset);
     self.artSizes(series.artSizes);
